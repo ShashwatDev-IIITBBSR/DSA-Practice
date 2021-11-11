0e64fa8ef579e1981313668b480acdae40c0d624
Shashwat Dev <shashwat.dev.iiit.bhubaneswar@gmail.com>
2021-11-11 20:57:10 +0530

    initial commit

---
+++ Linked List/tempCodeRunnerFile.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+struct node 
+{
+    int data;
+    struct node *next;
+};
+struct node *input()
+{
+    struct node *l,*k;
+    int i,s;
+    struct node *temp;
+    cout<<"Enter elements(use '0' to stop)";
+    l=new node;
+    l->next=NULL;
+    cin>>l->data;
+    k=l;
+    while(true)
+    {
+        temp=new node;
+        cin>>s;
+        if(s==0)
+        {
+            break;
+        }
+        temp->data=s;
+        l->next=temp;
+        temp->next=NULL;
+        l=temp;
+    }
+    l->next=NULL;
+    return k;
+}
+void display_linked_list(struct node *n)
+{
+    while(true)
+    {
+        if(n==NULL)
+        {
+            break;
+        }
+        cout<<n->data<<" ";
+        n=n->next;
+    }
+}
+int main()
+{
+    struct  node *first;
+    first=input();
+    display_linked_list(first);
+}
